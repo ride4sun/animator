cbbcf50e7b1a4174e92eee6661f721a6eddb3566
Joerg <ride4sun@gmail.com>
2020-07-07 15:04:27 -0700

    commented out log messages

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Flutter SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\flutter\_libraries\_animator\animator\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/jay/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jay/Desktop/Projects/obdchk/animator/example"
+export "FLUTTER_TARGET=/Users/jay/Desktop/Projects/obdchk/animator/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\Flutter SDK\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jay/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
+export "DART_DEFINES=flutter.inspector.structuredErrors=true"
